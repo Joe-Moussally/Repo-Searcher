b460c2448326b496d9f7f65cab6edec5ff12785a
MJoe111 <joemoussally111@gmail.com>
2022-10-27 16:01:43 +0300

    Fixing error styling condition

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -4,9 +4,10 @@ function EmptyState({text,error = false}) {
   return (
     <div 
       className={
-        error?
-        'text-red-500 flex items-center text-4xl font-bold mt-20':
-        'text-gray-400 flex items-center text-4xl font-bold mt-20'
+        ' flex items-center text-4xl font-bold mt-20'+
+        (error)?
+        'text-red-500':
+        'text-gray-400'
       }
     >
       <AiFillExclamationCircle className='mt-1 mx-4' size={46}/>
